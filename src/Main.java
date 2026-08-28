95481fac4f6acad6139bd362a2521d613219ce13
CristhianGarcia2 <cgarcia@uniempresarial.edu.co>
2026-08-28 18:31:57 -0500

    Instanciar objetos de subclases en la clase Main, Se crea la clase ejecutable y se instancian los objetos perro, gato y ave

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        perro miPerro = new perro("Firulais", 4, "15kg", "Labrador", "Grande");
+        gato miGato = new gato("Michi", 2, "4kg", "Siames", "Blanco y Gris");
+        ave miAve = new ave("Lucas", 1, "300g", "Canario", true);
+    }
+}
