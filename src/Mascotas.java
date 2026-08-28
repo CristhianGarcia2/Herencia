a49fb44abe708a3a29ef9a3ca9148ee98cd00038
CristhianGarcia2 <cgarcia@uniempresarial.edu.co>
2026-08-28 18:01:50 -0500

    Crear la superclase Mascotas con atributos privados y el metodo constructor

---
+++ src/Mascotas.java
@@ -0,0 +1,13 @@
+public class Mascotas {
+    private String nombre;
+    private int edad;
+    private String peso;
+    private String raza;
+
+    public Mascotas(String nombre, int edad, String peso, String raza) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.peso = peso;
+        this.raza = raza;
+    }
+}
