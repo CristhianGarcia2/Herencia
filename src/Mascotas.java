1de623413c8d33fd2c672ceaa35c6ec32b7e80be
CristhianGarcia2 <cgarcia@uniempresarial.edu.co>
2026-08-28 18:09:48 -0500

    Se agregan los  métodos getters y setters a la superclase Mascotas

--- src/Mascotas.java
+++ src/Mascotas.java
@@ -9,5 +9,18 @@ public class Mascotas {
         this.edad = edad;
         this.peso = peso;
         this.raza = raza;
+
     }
+    // Getters y Setters
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public int getEdad() { return edad; }
+    public void setEdad(int edad) { this.edad = edad; }
+
+    public String getPeso() { return peso; }
+    public void setPeso(String peso) { this.peso = peso; }
+
+    public String getRaza() { return raza; }
+    public void setRaza(String raza) { this.raza = raza; }
 }
